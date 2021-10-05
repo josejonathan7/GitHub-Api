65a285f53a6fb80105c36bd954ae65c0c2ff9ab1
José Jonathan <josejonathan689@gmail.com>
2021-10-05 00:35:14 -0300

    primneiro commit, inicialização do projeto, configuração do axios para chamada do github api, e criação de uma estrutura básica do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from './components/layout';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Layout>
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/71531287?v=4" alt="Avatar of user" />
+            <h1>José Jonathan</h1>
+            <h3>userName: </h3>
+            <span>josejonathan7</span>
+            <div>
+              <h4>Followers</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starreds</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>5</span>
+            </div>
+        </div>
+        <div>Repo</div>
+        <div>Started</div>
+      </Layout>
+    </main>
   );
 }
 
---
+++ src/components/layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Layout = ({ children }) => {
+
+
+    return (
+        <section>
+            <header>header</header>
+            { children }
+        </section>
+    )
+}
